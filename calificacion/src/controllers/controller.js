fc33f62c4310cfde32d44af896414afb7fc65e92
ddelgadillod <ddelgadillo@agrosavia.co>
2025-09-19 21:55:34 -0500

    consulta ultiam calificación y reporte calificación

--- calificacion/src/controllers/controller.js
+++ calificacion/src/controllers/controller.js
@@ -1,2 +1,29 @@
 import Calificacion from '../models/model.js';
 
+
+export async function ultimaCalificacion(req, res) {
+    const evaluadoId = req.body.evaluadoId || null;
+    console.log("Evaluado:" + evaluadoId);;
+  try {
+
+    const calificacion = await Calificacion.ultimaCalificacion(evaluadoId);
+    res.status(201).json(calificacion);
+  } catch (err) {
+    console.log("ERROR:: " + err)
+    res.status(500).json({ error: 'Error finding scores for user' });
+  }
+}
+
+
+export async function verHistorial(req, res) {
+    const evaluadoId = req.body.evaluadoId || null;
+    console.log("Evaluado:" + evaluadoId);
+  try {
+
+    const calificacion = await Calificacion.verHistorial(evaluadoId);
+    res.status(201).json(calificacion);
+  } catch (err) {
+    console.log("ERROR:: " + err)
+    res.status(500).json({ error: 'Error finding scores for user' });
+  }
+}
--- calificacion/src/routes/routes.js
+++ calificacion/src/routes/routes.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
-import {  } from '../controllers/controller.js';
+import { ultimaCalificacion } from '../controllers/controller.js';
+import { verHistorial } from '../controllers/controller.js';
 
 const router = Router();
+
+router.post('/verhistorial', verHistorial);
+router.post('/ultimacalificacion', ultimaCalificacion);
+
 
 export default router;
